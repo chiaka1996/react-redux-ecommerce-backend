a4ce98c5a9ed34041221d549de54b3b7ffbfddab
newton <chiakajunior@yahoo.com>
2020-09-02 13:37:03 +0100

    further commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port;
 
 
 app.use(cors());
